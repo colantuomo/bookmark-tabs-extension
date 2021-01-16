fe8b812ee5a2d671b721de012696a09ca6c24ec0
unknown <pcolantuomo@gmail.com>
2021-01-16 02:55:47 -0300

    feat: adding a option to close all tabs after save then

--- src/popup.js
+++ src/popup.js
@@ -1,31 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btnSaveTabs = document.getElementById("btnSaveTabs");
+  const canClose = document.getElementById("check");
+  const bookmarkInput = document.getElementById("bookmark");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const btnSaveTabs = document.getElementById('btnSaveTabs');
-
-    btnSaveTabs.addEventListener('click', () => {
-        const bookmark = document.getElementById('bookmark').value;
-        chrome.tabs.getAllInWindow(null, (tabs) => {
-            createFolder(bookmark, tabs);
-        });
+  btnSaveTabs.addEventListener("click", () => {
+    chrome.tabs.getAllInWindow(null, (tabs) => {
+      createFolder(bookmarkInput.value, tabs);
     });
+  });
 
-    const createFolder = (bookmark, tabs) => {
-        const dt = new Date();
-        return chrome.bookmarks.create({
-            parentId: "1",
-            title: `${bookmark} ${dt.toLocaleDateString()}`
-        }, ({ id }) => addAllTabsToBookmark(tabs, id))
-    }
+  const createFolder = (bookmark, tabs) => {
+    const dt = new Date();
+    return chrome.bookmarks.create(
+      {
+        parentId: "1",
+        title: `${bookmark} - ${dt.toLocaleDateString()}`,
+      },
+      ({ id }) => addAllTabsToBookmark(tabs, id)
+    );
+  };
 
-    const addAllTabsToBookmark = (tabs, parentId) => {
-        tabs.forEach((tab) => {
-            const { title, url } = tab;
-            chrome.bookmarks.create({
-                parentId,
-                title,
-                url
-            }, () => window.close())
-        })
-    }
+  const closeAlltabs = (tab) => {
+    chrome.tabs.query({ active: true }, (activeTabs) => {
+      currentTab = activeTabs[0];
+      if (currentTab.id !== tab.id) {
+        chrome.tabs.remove(tab.id, () => {});
+      }
+    });
+  };
 
+  const addAllTabsToBookmark = (tabs, parentId) => {
+    tabs.forEach((tab) => {
+      const { title, url } = tab;
+      chrome.bookmarks.create(
+        {
+          parentId,
+          title,
+          url,
+        },
+        () => window.close()
+      );
+      if (canClose.checked) closeAlltabs(tab);
+    });
+  };
 });
